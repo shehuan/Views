9c4aae9d82dcabe5326176c48f9dd670663c1fc6
Othershe <shehuan320@163.com>
2016-07-05 15:04:11 +0800

    JumpTextView

---
+++ app/src/main/java/com/othershe/views/view/JumpTextView.java
@@ -0,0 +1,39 @@
+package com.othershe.views.view;
+
+import android.os.Handler;
+import android.os.Message;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.RelativeSizeSpan;
+import android.widget.TextView;
+
+/**
+ * Created by Administrator on 2016/7/5.
+ */
+public class JumpTextView {
+//    textView = (TextView) findViewById(R.id.textview);
+//    handler.sendEmptyMessage(10086);
+
+//    private int position;
+//    Handler handler = new Handler() {
+//
+//        @Override
+//        public void handleMessage(Message msg) {
+//            super.handleMessage(msg);
+//            switch (msg.what) {
+//                case 10086:
+//                    SpannableString spannableString = new SpannableString("非专业程序员！");
+//
+//                    RelativeSizeSpan sizeSpan = new RelativeSizeSpan(1.2f);
+//                    spannableString.setSpan(sizeSpan, position, position + 1, Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
+//                    textView.setText(spannableString);
+//                    position++;
+//                    if(position >= textView.getText().toString().length()) {
+//                        position = 0;
+//                    }
+//                    handler.sendEmptyMessageDelayed(10086, 200);
+//                    break;
+//            }
+//        }
+//    };
+}
